e9a4eddabdfc52140970d0ab106e1fcba9410810
Aryan Karfa <xtrmakarov@gmail.com>
2026-07-03 12:28:19 +0530

    Delete backend directory

--- backend/src/constants/rawg.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = 'https://api.rawg.io/api';
-export const DEFAULT_ORDERING = '-rating';
-export const DEFAULT_PAGE = 1;
-export const DEFAULT_LIMIT = 20;
-export const MAX_LIMIT = 40;
-export const TIMEOUT = 5000;
